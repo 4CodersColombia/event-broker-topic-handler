1fcac0b20a43e4e1624f747bceed35849948e130
Andres Camilo Gonzalez <acgonzalezh@correo.udistrital.edu.co>
2021-07-28 15:37:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 
 setup(
     name='topic_handler',
-    version='0.0.6',
+    version='0.1.0',
     include_package_data=True,
     description='Library to  management kafka topics with faust',
     author='Andres Gonzalez',
@@ -12,15 +12,15 @@ setup(
     license="GPLv3",
     # use the URL to the github repo
     url='https://github.com/4CodersColombia/event-broker-topic-handler',
-    download_url='https://github.com/4CodersColombia/event-broker-topic-handler/archive/refs/tags/0.0.6.tar.gz',
+    download_url='https://github.com/4CodersColombia/event-broker-topic-handler/archive/refs/tags/0.1.0.tar.gz',
     keywords=['kafka', 'faust'],
     classifiers=['Programming Language :: Python :: 3.9', ],
     packages=setuptools.find_packages(),
     install_requires=[
         'kafka-python==1.4.6',
-        'betterproto == 1.2.5',
-        'PyJWT == 1.7.1',
-        'cryptography == 3.4.7'
+        'betterproto==1.2.5',
+        'PyJWT==1.7.1',
+        'cryptography==3.4.7'
     ],
 )
 
